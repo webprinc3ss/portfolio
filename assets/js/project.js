2a472a4d85b201d48b0eb1610bbae0e188683cdc
Mystic Kitty <djabranton@gmail.com>
2021-04-09 10:51:23 -0700

    Created modal script.

---
+++ assets/js/project.js
@@ -0,0 +1,28 @@
+//Get modal Element
+var modal = document.getElementById('simpleModal');
+//Get open Modal Button
+var modalBtn = document.getElementById('modalBtn');
+//Get close Btn
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+//Listen for open click
+modalBtn.addEventListener('click', openModal);
+//Listen for close click
+closeBtn.addEventListener('click', closeModal);
+//Listen for outside click
+window.addEventListener('click', outsideClick);
+
+//Function to open Modal
+function openModal() {
+    modal.style.display = 'block';
+};
+//Function to close Modal
+function closeModal() {
+    modal.style.display = 'none';
+};
+//Function to close Modal if outside click
+function outsideClick(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+};
