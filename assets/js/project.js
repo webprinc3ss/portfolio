b6c1413faa5b53f0abc7222bba5652898af8f5e7
Mystic Kitty <djabranton@gmail.com>
2021-04-09 12:22:59 -0700

    Added modal to one project.

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,3 +1,4 @@
+//Begin Modal Popup Code
 //Get modal Element
 var modal = document.getElementById('simpleModal');
 //Get open Modal Button
@@ -26,3 +27,4 @@ function outsideClick(e) {
         modal.style.display = 'none';
     }
 };
+//End Modal Popup Code
